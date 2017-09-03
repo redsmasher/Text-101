e08deba933f96dc3a383218c15e5fd52070eaa9f
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-03 17:06:47 +0300

    Text101 1: Adding user interface text and basic text controller

---
+++ Text101(Horror)/Assets/Scripts/TextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextController : MonoBehaviour 
+{
+	public Text text;
+
+	void Start ()
+	{
+		text.text = "Hello world";
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			text.text = "Space key pressed";
+		}
+	}
+}
