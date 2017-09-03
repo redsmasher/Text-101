07a511e5dad2571ca813211cec4c038166771517
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-03 17:25:59 +0300

    Text 101: 2 Building the Game's Engine

--- Text101(Horror)/Assets/Scripts/TextController.cs
+++ Text101(Horror)/Assets/Scripts/TextController.cs
@@ -7,18 +7,47 @@ public class TextController : MonoBehaviour
 {
 	public Text text;
 
+	private enum States {cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, freedom};
+	private States myState;
+
 	void Start ()
 	{
-		text.text = "Hello world";
+		myState = States.cell;
 	}
 
 	void Update ()
 	{
-		if (Input.GetKeyDown (KeyCode.Space)) 
+		print (myState);
+		if (myState == States.cell) 
 		{
-			text.text = "You are in a prison cell and you want to escape, use everything you can find to escape!"+
-						" There are some dirty sheets in the bed, a mirror in the wall and some filthy tools on the floor\n\n"+
-						"Press V to view Sheets, M to view Mirror, T to view tools";
+			state_cell ();
+		}
+		else if(myState == States.sheets_0)
+		{
+			state_sheets0 ();
+		}
+	}
+
+	void state_cell()
+	{
+		text.text = "You are in a prison cell and you want to escape, use everything you can find to escape!"+
+		" There are some dirty sheets in the bed, a mirror in the wall and some filthy tools on the floor\n\n"+
+		"Press S to view Sheets, M to view Mirror, T to view tools";
+
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			myState = States.sheets_0;
+		}
+	}
+
+	void state_sheets0()
+	{
+		text.text = "I'm tired of sleeping on these things!\n\n"+
+					"Press R to return";
+
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			myState = States.cell;
 		}
 	}
 }
