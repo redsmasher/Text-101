a28068bec026d7da07d038ea21e3a6e095cc8574
DESKTOP-9EJ05EC\ichig <ichigo77@hotmail.gr>
2017-09-03 17:13:59 +0300

    Text 101 2:Starting the Game Story

--- Text101(Horror)/Assets/Scripts/TextController.cs
+++ Text101(Horror)/Assets/Scripts/TextController.cs
@@ -16,7 +16,9 @@ public class TextController : MonoBehaviour
 	{
 		if (Input.GetKeyDown (KeyCode.Space)) 
 		{
-			text.text = "Space key pressed";
+			text.text = "You are in a prison cell and you want to escape, use everything you can find to escape!"+
+						" There are some dirty sheets in the bed, a mirror in the wall and some filthy tools on the floor\n\n"+
+						"Press V to view Sheets, M to view Mirror, T to view tools";
 		}
 	}
 }
